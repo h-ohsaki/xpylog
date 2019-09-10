175934bb00973d1b92b8b363a1b087fb0fbe67c2
Hiroyuki Ohsaki <code@lsnl.jp>
2019-09-11 02:49:10 +0900

    version 1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='xpylog',
-    version='1.1',
+    version='1.2',
     author='Hiroyuki Ohsaki',
     author_email='ohsaki@lsnl.jp',
     description='An xconsole-like syslog monitor on X11',
